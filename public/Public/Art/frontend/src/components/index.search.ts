c28bbdfaf333f0e65928729db1394a2eb1e4cd07
cryptonique0 <abdulganiyu838@gmail.com>
2026-01-26 23:24:43 +0100

    chore: add public/Public/Art/frontend/src/components/index.search.ts

---
+++ public/Public/Art/frontend/src/components/index.search.ts
@@ -0,0 +1,9 @@
+/**
+ * Advanced Search Components
+ * Index file for all search-related UI components
+ */
+
+export { AdvancedSearch } from './AdvancedSearch';
+export { FilterPanel } from './FilterPanel';
+export { TrendingNFTs } from './TrendingNFTs';
+export { RecentlyAdded } from './RecentlyAdded';
