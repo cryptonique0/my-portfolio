31c34bb11300308208e6ddff322ffd502daaba70
cryptonique0 <abdulganiyu838@gmail.com>
2026-01-26 23:24:45 +0100

    chore: add public/Public/Art/frontend/src/pages/TransactionHistoryPage.tsx

---
+++ public/Public/Art/frontend/src/pages/TransactionHistoryPage.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import TransactionHistory from '../components/transactions/TransactionHistory';
+
+export default function TransactionHistoryPage() {
+  return <TransactionHistory />;
+}
