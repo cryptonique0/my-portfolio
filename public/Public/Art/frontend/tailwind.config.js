80c9dcd274c27e5b9099a2c670b2f0fa5f96dfbd
cryptonique0 <abdulganiyu838@gmail.com>
2026-01-26 23:24:46 +0100

    chore: add public/Public/Art/frontend/tailwind.config.js

---
+++ public/Public/Art/frontend/tailwind.config.js
@@ -0,0 +1,41 @@
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          50: '#fdf4ff',
+          100: '#fae8ff',
+          200: '#f5d0ff',
+          300: '#f0abff',
+          400: '#e879f9',
+          500: '#d946ef',
+          600: '#c026d3',
+          700: '#a21caf',
+          800: '#86198f',
+          900: '#6b21a8',
+        },
+        secondary: {
+          50: '#fdf2f8',
+          100: '#fce7f3',
+          200: '#fbcfe8',
+          300: '#f8b4d6',
+          400: '#f472b6',
+          500: '#ec4899',
+          600: '#db2777',
+          700: '#be185d',
+          800: '#9d174d',
+          900: '#831843',
+        },
+      },
+      animation: {
+        'pulse-slow': 'pulse 3s cubic-bezier(0.4, 0, 0.6, 1) infinite',
+      },
+    },
+  },
+  plugins: [],
+  darkMode: 'class',
+}
