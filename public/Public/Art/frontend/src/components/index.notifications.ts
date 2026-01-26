cf36b065cf2f418fb9fc544e0c51c6be5002cedc
cryptonique0 <abdulganiyu838@gmail.com>
2026-01-26 23:24:43 +0100

    chore: add public/Public/Art/frontend/src/components/index.notifications.ts

---
+++ public/Public/Art/frontend/src/components/index.notifications.ts
@@ -0,0 +1,11 @@
+/**
+ * Notification Center Components & Hooks
+ * Index file for easy importing
+ */
+
+export { NotificationBell } from './NotificationBell';
+export { NotificationCenter } from './NotificationCenter';
+export { NotificationPreferences } from './NotificationPreferences';
+
+export type { Notification, NotificationPreferences } from '../hooks/useNotificationCenter';
+export { useNotificationCenter, useNotificationPreferences } from '../hooks/useNotificationCenter';
